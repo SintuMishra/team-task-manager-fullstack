29f1a4f0176ebdb26c8dbf1dc08b60f06ad1f721
Sintu Mishra <169442032+SintuMishra@users.noreply.github.com>
2026-05-01 10:05:56 +0530

    Fix server env loading path

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -1,11 +1,21 @@
 import dotenv from "dotenv";
+import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+const serverEnvPath = path.resolve(__dirname, "../../.env");
+const rootEnvPath = path.resolve(__dirname, "../../../.env");
+
+if (fs.existsSync(serverEnvPath)) {
+  dotenv.config({ path: serverEnvPath });
+} else if (fs.existsSync(rootEnvPath)) {
+  dotenv.config({ path: rootEnvPath });
+} else {
+  dotenv.config();
+}
 
 const frontendOrigins = (process.env.FRONTEND_URL || "http://localhost:5173")
   .split(",")
